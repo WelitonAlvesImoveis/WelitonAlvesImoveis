7b5bc6b6962c5467e6f2bb1ea88db8a2ed5a3dab
WilliamZambom <wilzambom@gmail.com>
2026-08-11 18:06:12 -0300

    URL render config ..

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "https://welitonalvesimoveis.onrender.com/api";
+    : "https://welitonalvesimoveis.onrender.com";
