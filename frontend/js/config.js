999e3f72fdea4897a1dbd64bb1ca1058bb78f071
WilliamZambom <wilzambom@gmail.com>
2026-08-11 18:03:31 -0300

    URL render config ..

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
 const API_BASE_URL =
   location.hostname === "localhost" || location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "https://property-manager-evui.onrender.com";
+    : "https://welitonalvesimoveis.onrender.com/api";
